e7202a17f7e4fd991379daa977fdb3223bde3f84
Uthaeus <romanlavery@gmail.com>
2023-05-12 20:48:17 -0600

    installed carrierwave, added validations and mounted uploaders in models

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,2 +1,7 @@
 class Meetup < ApplicationRecord
+
+    validates_presence_of :title, :description, :location, :date, :time
+
+    mount_uploader :main_image, MeetupUploader
+    mount_uploader :thumb_image, MeetupUploader
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+    validates_presence_of :title, :body
+
+    mount_uploader :image, PostUploader
 end

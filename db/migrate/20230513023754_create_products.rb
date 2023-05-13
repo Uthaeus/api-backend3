413eb7a4b17f9ab8d89b6fa0b5e0d13028d1b930
Uthaeus <romanlavery@gmail.com>
2023-05-12 20:38:43 -0600

    generated scaffolds for posts, meetups, and products

---
+++ db/migrate/20230513023754_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :price
+      t.string :main_image
+      t.string :thumb_image
+
+      t.timestamps
+    end
+  end
+end
